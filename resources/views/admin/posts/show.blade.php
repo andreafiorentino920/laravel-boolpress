8e944bc38d427805f512d9dd1f9254148e2d241e
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-11-22 14:24:24 +0100

    Aggiunti tag nella show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -9,6 +9,15 @@
 
                 <div class="card-body">
                     <h1>{{$post["title"]}}</h1>
+                    @if ($post["category"])
+                    <h3>Categoria: {{$post["category"]["name"]}}</h3>
+                    @endif
+                    @if ($post["tags"])
+                    <h3>Tags:</h3>
+                    @foreach ($post["tags"] as $tag)
+                        <span class="badge badge-primary">{{$tag["name"]}}</span>
+                    @endforeach
+                    @endif
                     <p>{{$post["content"]}}</p>
                 </div>
             </div>
